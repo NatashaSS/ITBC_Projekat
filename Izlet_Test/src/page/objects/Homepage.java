099831c6620823c8bebe961ddc477de44248881d
natasas <natasha1stevanovic@gmail.com>
2019-04-06 19:27:10 +0200

    subota update

--- Izlet_Test/src/page/objects/Homepage.java
+++ Izlet_Test/src/page/objects/Homepage.java
@@ -1,32 +1,28 @@
 package page.objects;
+
 import org.openqa.selenium.*;
 
 public class Homepage {
-			public static final String PAGE_URL="http://localhost/izlet/index.php";
-		public static final String GALLERY="//nav[@class='main-nav']//a[@class='a_bold'][contains(text(),'Galerija')]";
-		
-//		metoda koja preuzima i cuva Gallery link xpath
-		public static WebElement getGalleryLink(WebDriver dr) {
-			WebElement wb=dr.findElement(By.xpath(GALLERY));
-			return wb;
-		}
-		
-//		metoda koja izvrsava klik na Gallery button element, uz pomoc prethodno pronadjenog xpath-a
-		public static void clickGalleryLink(WebDriver dr) {
-			getGalleryLink(dr).click();
-		}
-		
-	
-	
-		
-// otvara homepage
-		public static void openPage(WebDriver dr) {
-			dr.get(PAGE_URL);
-		}
-		
-//		reloaduje stranicu?
-		public static void navigateTo(WebDriver dr) {
-			dr.navigate().to(PAGE_URL);
-		}
+	public static final String PAGE_URL = "http://localhost/izlet/index.php";
+	public static final String GALLERY = "//nav[@class='main-nav']//a[@class='a_bold'][contains(text(),'Galerija')]";
+
+// Homepage 
+	public static void openPage(WebDriver dr) {
+		dr.get(PAGE_URL);
 	}
 
+	public static void navigateTo(WebDriver dr) {
+		dr.navigate().to(PAGE_URL);
+	}
+
+//		 Gallery link methods
+	public static WebElement getGalleryLink(WebDriver dr) {
+		WebElement wb = dr.findElement(By.xpath(GALLERY));
+		return wb;
+	}
+
+	public static void clickGalleryLink(WebDriver dr) {
+		getGalleryLink(dr).click();
+	}
+
+}
